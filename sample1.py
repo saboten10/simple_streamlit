74056569eb71ddc98cf130cafc03bd6d98838dcf
齊藤靖 <saboten@saboten-no-MacBook-Pro.local>
2022-01-25 20:30:26 +0900

    1st commit

---
+++ sample1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import streamlit as st
+
+
+def main():
+    # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
+
+    st.title('シンプルstreamlitアプリ')
+    st.markdown('** Hellow World!!')
+#   
+if __name__ == '__main__':
+    main()
