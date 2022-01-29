05c96d02110389aee2445197741eb8b3ecf332d0
Yasushi <saboten@saboten-no-MacBook-Pro.local>
2022-01-29 15:37:55 +0900

    4th commit

---
+++ sample1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import streamlit as st
+
+
+def main():
+    # Streamlit code
+
+    st.title('シンプルな　streamlitアプリです')
+    st.markdown('** Hellow World!!')
+#   
+if __name__ == '__main__':
+    main()
